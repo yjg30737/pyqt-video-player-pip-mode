c86aa61a296d1ea7f18dc694cef07a1cdadead25
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-18 11:22:02 +0900

    Remove button.css which is not used anymore

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_video_player_pip_mode.style': ['button.css', 'progressbar.css'],
+    package_data={'pyqt_video_player_pip_mode.style': ['progressbar.css'],
                   'pyqt_video_player_pip_mode.ico': ['pip.svg', 'close.svg', 'play.svg', 'pause.svg']},
     description='PyQt Video Player PIP Mode',
     url='https://github.com/yjg30737/pyqt-video-player-pip-mode.git',
